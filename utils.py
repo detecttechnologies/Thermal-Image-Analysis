2cb94501cda8ec3ccab8f147323103ba759192d3
Lemorra <kochutanjinga@gmail.com>
2022-02-08 18:00:55 +0530

    Added support for export as .tiff, .csv files

--- utils.py
+++ utils.py
@@ -211,62 +211,109 @@ def saveImage(window):
     overlays = window.overlays
     exthandler = window.exthandler
     Tk().withdraw()
-    file = filedialog.asksaveasfile(
-        filetypes=[("PNG Image", "*.png")], defaultextension=".png"
+    file = filedialog.asksaveasfilename(
+        filetypes=[("PNG Image", "*.png"),("TIFF Image", "*.tiff"),("CSV File", "*.csv")]
     )
 
     if file:
-        filename = file.name
+        filename = file.split("/")[-1]
 
-        if (
-            messagebox.askquestion(
-                "Save options", "Do you want to save with the annotations?"
+        if filename.endswith('.tiff'):
+            if (messagebox.askquestion("Before we proceed",f"Are you sure you want to export image as {filename}?") == "yes"):
+                tiffExport(window, file)
+            else:
+                messagebox.showinfo("Export Cancelled", f"You have cancelled the process of exporting {filename}")
+
+        elif filename.endswith('.csv'):
+            if (messagebox.askquestion("Before we proceed",f"Are you sure you want to export image as {filename}?") == "yes"):
+                csvExport(window, file)
+            else:
+                messagebox.showinfo("Export Cancelled", f"You have cancelled the process of exporting {filename}")
+
+        else:
+            print(file)
+            pngExport(window, file, imageSurface, overlays, exthandler)
+
+def pngExport(window, filename, imageSurface, overlays, exthandler):
+    """Function to export image as .PNG format
+
+    Args:
+        window (class Window object): object that refers to main window
+        filename (str): Filename with extension. Eg: example.txt
+        imageSurface (pygame.SURFACE): [description]
+        overlays ([type]): [description]
+        exthandler ([type]): [description]
+    """
+    if (
+        messagebox.askquestion(
+            "Save options", "Do you want to save with the annotations?"
+        )
+        == "yes"
+    ):
+        imageSurface.blit(overlays, (0, 0))
+
+        data = SaveData()
+        data.plots = window.exthandler.plots
+        data.rects = window.exthandler.rects
+        data.tableEntries = []
+        data.mat = window.mat
+        data.mat_orig = window.mat_orig
+        data.mat_emm = window.mat_emm
+        data.raw = window.raw
+        data.meta = window.meta
+        data.overlays = pygame.image.tostring(overlays, "RGBA")
+
+        if exthandler.mainFigure:
+            exthandler.mainFigure.saveFig(
+                ".".join(filename.split(".")[:-1]) + "_plot.png"
             )
-            == "yes"
-        ):
-            imageSurface.blit(overlays, (0, 0))
+        if exthandler.mainTable:
+            with pd.ExcelWriter(
+                ".".join(filename.split(".")[:-1]) + "_values.xlsx",
+                engine="xlsxwriter",
+            ) as writer:
+                exthandler.mainTable.getDF().to_excel(writer, sheet_name="Table")
+                if len(data.plots) > 0:
+                    pd.DataFrame(
+                        [plot[1] for plot in data.plots],
+                        index=[f"l{i+1}" for i in range(len(data.plots))],
+                    ).to_excel(writer, sheet_name="Lines")
+                data.tableEntries = exthandler.mainTable.data
+                for i, (x1, x2, y1, y2) in enumerate(data.rects):
+                    pd.DataFrame(
+                        data.mat_emm[y1:y2, x1:x2],
+                        index=y1 + np.arange(y2 - y1),
+                        columns=x1 + np.arange(x2 - x1),
+                    ).to_excel(writer, sheet_name=f"Area{i+1}")
 
-            data = SaveData()
-            data.plots = window.exthandler.plots
-            data.rects = window.exthandler.rects
-            data.tableEntries = []
-            data.mat = window.mat
-            data.mat_orig = window.mat_orig
-            data.mat_emm = window.mat_emm
-            data.raw = window.raw
-            data.meta = window.meta
-            data.overlays = pygame.image.tostring(overlays, "RGBA")
+        with open(".".join(filename.split(".")[:-1]) + ".pkl", "wb") as f:
+            pickle.dump(data, f, -1)
 
-            if exthandler.mainFigure:
-                exthandler.mainFigure.saveFig(
-                    ".".join(filename.split(".")[:-1]) + "_plot.png"
-                )
-            if exthandler.mainTable:
-                with pd.ExcelWriter(
-                    ".".join(filename.split(".")[:-1]) + "_values.xlsx",
-                    engine="xlsxwriter",
-                ) as writer:
-                    exthandler.mainTable.getDF().to_excel(writer, sheet_name="Table")
-                    if len(data.plots) > 0:
-                        pd.DataFrame(
-                            [plot[1] for plot in data.plots],
-                            index=[f"l{i+1}" for i in range(len(data.plots))],
-                        ).to_excel(writer, sheet_name="Lines")
-                    data.tableEntries = exthandler.mainTable.data
-                    for i, (x1, x2, y1, y2) in enumerate(data.rects):
-                        pd.DataFrame(
-                            data.mat_emm[y1:y2, x1:x2],
-                            index=y1 + np.arange(y2 - y1),
-                            columns=x1 + np.arange(x2 - x1),
-                        ).to_excel(writer, sheet_name=f"Area{i+1}")
+    imgdata = pygame.surfarray.pixels3d(imageSurface).astype(np.uint8)
+    imgdata = np.swapaxes(imgdata, 0, 1)
+    Image.fromarray(imgdata).save(filename)
+    print("Saved successfully")
+        
+def tiffExport(window, filename):
+    """Function to export as .tiff File
 
-            with open(".".join(filename.split(".")[:-1]) + ".pkl", "wb") as f:
-                pickle.dump(data, f, -1)
+    Args:
+        window (class Window object): Window object that refers to main window
+        filename (str): Filename with full path and extension. Eg: /home/user/hello/example.txt
+    """
+    imgKelvin = Image.fromarray(window.thermalData)
+    imgKelvin.save(filename)
+    print(f"Sucessfully saved {filename} as tiff file")
 
-        imgdata = pygame.surfarray.pixels3d(imageSurface).astype(np.uint8)
-        imgdata = np.swapaxes(imgdata, 0, 1)
-        Image.fromarray(imgdata).save(filename)
-        print("Saved successfully")
+def csvExport(window, filename):
+    """Function to export as .csv File
+
+    Args:
+        window (class Window object): Window class object that refers to main window
+        filename (str): Filename with full path and extension. Eg: /home/user/hello/example.txt
+    """
+    np.savetxt(filename, window.thermalData, delimiter=",")
+    print(f"Sucessfully saved {filename} as .csv file")
 
 
 def openImage():
